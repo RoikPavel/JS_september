690b2d1265c64264fd72f106118d2e27e52a7b80
RoikPavel <roik@i.ua>
2021-11-16 22:42:04 +0200

    add templates cw lesson 3

---
+++ lesson3/cw/template 1.1/template1_1.js
@@ -0,0 +1,7 @@
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (let items of listOfItems) {
+    document.write(`<ul>
+        <li>${items}</li>
+    </ul>`);
+}
