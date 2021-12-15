1683d8f8f9cd18ef68800ac7e13f64ce52fb1782
RoikPavel <roik@i.ua>
2021-12-15 11:31:59 +0200

    cw lesson 12 оновлено структуру

--- lesson12/cw/cw_lesson12.js
+++ lesson12/cw/cw_lesson12.js
@@ -3,37 +3,36 @@ https://jsonplaceholder.typicode.com/posts
     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі
     поточного поста*/
 
-fetch('https://jsonplaceholder.typicode.com/posts').then(response=>response.json())
-    .then(posts=>{
-        let div = document.createElement('div');
+fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
+    .then(posts => {
+        let wrap = document.createElement('div');
         for (let post of posts) {
-            console.log(post);
-        let userId = document.createElement('div');
-        userId.innerText = `userID: ${post.userId}`;
+            let userId = document.createElement('div');
+            userId.innerText = `USER ID - ${post.userId}`;
             let id = document.createElement('div');
-            id.innerText = `ID: ${post.id}`;
-            let title = document.createElement('h3');
-            title.innerText = post.title;
-            let body = document.createElement('p');
-            body.innerText = post.body;
+            id.innerText = `ID - ${post.id}`;
+            let title = document.createElement('div');
+            title.innerText = `TITLE - ${post.title}`;
+            let body = document.createElement('div');
+            body.innerText = `BODY ${post.body}`;
             let btn = document.createElement('button');
-            btn.innerText = 'show comment';
-            btn.onclick = function (){{
-                fetch('https://jsonplaceholder.typicode.com/comments')
-                    .then(response => response.json())
+            btn.innerText = 'comments';
+            let allComments = document.createElement('div');
+            let hr = document.createElement('hr');
+            btn.onclick = function () {
+                fetch('https://jsonplaceholder.typicode.com/comments').then(response => response.json())
                     .then(comments => {
                         for (const comment of comments) {
-                            if (post.id === comment.id) {
-                                body.innerText = comment.body;
+                            if (post.id === comment.postId) {
+                                let aComment = document.createElement('div');
+                                aComment.innerText = (`POST ID - ${comment.postId}\n ID - ${comment.id}\n TITLE - ${comment.title}\n BODY - ${comment.body}\n`);
+
+                                allComments.appendChild(aComment);
                             }
                         }
-                    })
+                    });
             }
-                body.style.display = 'block';
-            };
-            let hr = document.createElement('hr');
-            div.append(userId, id, title, btn, body, hr);
+            wrap.append(userId, id, title, body, btn, allComments, hr);
         }
-        document.body.appendChild(div);
-
-    })
+        document.body.appendChild(wrap);
+    });
